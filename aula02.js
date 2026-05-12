d69adf20d781fefaa01dc3b57da508834af536f4
SEU MADROGA BOLADEZA <heros.maurer@escola.pr.gov.br>
2026-05-12 10:28:03 -0300

    Add files via upload

--- aula02.js
+++ aula02.js
@@ -1,23 +1,33 @@
 //DEFINIÇÃO DE VARIÁVEIS
 
+let pessoa = "nome da pessoa";
+let idadePessoa = 30;
+let alturaPessoa = 1.75;
+let statusPessoa = true;
+let salarioPessoa = 1000;
+let salarioBonus = 100;
 
+let salarioTotal = salarioBonus + salarioPessoa;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log( pessoa );
+console.log( idadePessoa );
+console.log( alturaPessoa );
+console.log( statusPessoa );
+console.log( salarioPessoa );
+console.log( salarioBonus );
+console.log( salarioTotal );
+ 
+//UNINDO TODAS AS INFORMAÇÕES EM UM TEXTO:
+console.log(
+'O nome digitado foi: '+pessoa+
+' a idade digitada foi: '+idadePessoa+
+' a altura digitada foi: '+alturaPessoa+
+' o status digitado foi: '+statusPessoa+    
+' o salário digitado foi: '+salarioPessoa+
+' o bônus digitado foi: '+salarioBonus+
+' o salário total é: '+salarioTotal+ '!!!'
+)
+ 
 
 
 
