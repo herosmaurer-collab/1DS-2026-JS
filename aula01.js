871b14d665112a69068d3b811cd81abbf515ee64
SEU MADROGA BOLADEZA <heros.maurer@escola.pr.gov.br>
2026-05-12 08:30:37 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,10 @@
+  //alert("o carpa capim fica batendo no amiguinho!!!")
+
+  console.log("Primeiro códigoem JS");
+  console.log(4+5);
+  console.log(3 * 4 );
+  console.log( true );
+  console.log( 3.89 );
+
+  console.log( "Heros" + " " +23+" " +1.75+" "+ true);
+
