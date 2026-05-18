613d10e5af86ce8f1fc635c94e8b62184965b3e2
SEU MADROGA BOLADEZA <heros.maurer@escola.pr.gov.br>
2026-05-18 09:22:01 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,20 @@
+let nome = "Heros"
+
+if (nome == "Heros ") {
+   console.log("Voce digitou o nome correto!")
+}else{
+    console.log("Voce digitou outra coisa...")
+}
+
+let idade = 18
+
+if(idade <= 18) {
+    console.log("Voce é praticamente adulto...")
+}else if( idade < 18 ){
+    console.log("Voce é menor de idade...")
+}else {
+    console.log("Voce é adulto...")
+}
+
+//FAÇA UMA VERIFICAÇÃO COM IF E ELSE
+//
